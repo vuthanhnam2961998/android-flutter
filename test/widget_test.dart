f870f7357687629cecbeab6c36c61cc9085ea734
vuthanhnam2961998 <49110652+vuthanhnam2961998@users.noreply.github.com>
2019-11-24 22:02:17 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 // vua thay doi
+// xoa dong tren di
 import 'package:flutter/material.dart';
 import 'package:flutter_hutech_food/src/app.dart';
 import 'package:flutter_test/flutter_test.dart';
